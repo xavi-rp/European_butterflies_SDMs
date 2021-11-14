8bf4e1c7e8536b590f3d6e1217ca32e82b47a5ec
xavi-rp <xavi.rotllan.puig@gmail.com>
2021-11-14 09:43:41 +0100

    ordering sps for modelling

--- SDMs_butterflies.R
+++ SDMs_butterflies.R
@@ -26,6 +26,9 @@ worldclim_all <- stack(worldclim_files)
 
 eur_coords <- c(-13, 48, 35, 72)
 worldclim_all <- crop(worldclim_all, eur_coords)
+writeRaster(worldclim_all, filename = "worldclim_all.tif")
+
+worldclim_all <- stack("worldclim_all.tif")
 worldclim_all
 plot(worldclim_all[[3]])
 
@@ -33,6 +36,9 @@ worldclim_all_data <- as.data.frame(worldclim_all)
 worldclim_all_data <- worldclim_all_data[complete.cases(worldclim_all_data), ]
 head(worldclim_all_data)
 nrow(worldclim_all_data)
+
+write.csv(worldclim_all_data, "worldclim_all_data.csv", row.names = FALSE)
+worldclim_all_data
 
 
 ## Background points ####
@@ -59,10 +65,12 @@ dev.off()
 occs_all <- read.csv("sp_records_20211109.csv", header = TRUE)
 head(occs_all)
 table(occs_all$species)
+tbl <- table(occs_all$species)
+ordr <- names(sort(tbl))
+spcies <- data.frame(taxons = unique(occs_all$sp2), sps = unique(occs_all$species))
+spcies <- spcies[match(ordr, spcies$sps),]
+taxons <- spcies$taxons
 
-taxons <- unique(occs_all$sp2)
-spcies <- data.frame(taxons, sps = unique(occs_all$species))
-spcies
 
 for (t in taxons){
   #print(t)
@@ -93,11 +101,11 @@ for (t in taxons){
   
   ## occurrences for training/testing
   sps_data <- stack(occs_i_rstr, worldclim_all) 
-  sps_data <- as.data.frame(sps_data)
-  sps_data$raster_position <- 1:nrow(sps_data)
+  sps_data <- as.data.table(as.data.frame(sps_data))
+  sps_data[, raster_position := 1:nrow(sps_data)]
   
   # data set for presences
-  sps_data_presences <- sps_data[sps_data$layer == 1, ]
+  sps_data_presences <- sps_data[layer == 1, ]
   sps_data_presences <- sps_data_presences[complete.cases(sps_data_presences), ]
   rm(sps_data); gc()
   
@@ -107,7 +115,7 @@ for (t in taxons){
   names(sps_data_absences)
   
   ## Running ENMeval (https://jamiemkass.github.io/ENMeval/articles/ENMeval-2.0.0-vignette.html)
-  modl <- ENMevaluate(occs = sps_data_presences[, names(sps_data_presences) %in% names(worldclim_all)], 
+  modl <- ENMevaluate(occs = sps_data_presences[, .SD, .SDcols = names(worldclim_all)], 
                       envs = NULL, 
                       bg = sps_data_absences[, names(sps_data_absences) %in% names(worldclim_all)], 
                       algorithm = 'maxnet', 
@@ -121,11 +129,12 @@ for (t in taxons){
                       parallel = TRUE,
                       numCores = 7
                       )
+  rm(sps_data_absences); gc()
   modl
   modl@results
-  View(modl@results)
+  #View(modl@results)
   write.csv(modl@results, file = paste0(dir2save, "ENMeval_results_", t, ".csv"))
-  evalplot.stats(e = modl, stats = "or.mtp", color = "fc", x.var = "rm")
+  #evalplot.stats(e = modl, stats = "or.mtp", color = "fc", x.var = "rm")
   
   occurrences_train <- modl@occs
   occurrences_test <- nrow(modl@occs.testing)  # none because crossvalidation
@@ -140,10 +149,10 @@ for (t in taxons){
   
   modl_args <- eval.models(modl)[[optimal$tune.args]]
   modl_args$betas
-  str(modl_args)
+  #str(modl_args)
   
-  dev.off()
-  pdf(paste0(dir2save, "opt_model_RespCurves", t, ".pdf"))
+  #dev.off()
+  pdf(paste0(dir2save, "opt_model_RespCurves_", t, ".pdf"))
   plot(modl_args, type = "cloglog")
   # And these are the marginal response curves for the predictor variables wit non-zero 
   # coefficients in our model. We define the y-axis to be the cloglog transformation, which
@@ -157,32 +166,43 @@ for (t in taxons){
   save(modl, file = paste0(dir2save, "opt_model_", t, ".RData"))
   
   # making predictions
-  worldclim_all_data_kk <- worldclim_all_data
+  worldclim_all_data <- fread("worldclim_all_data.csv", header = TRUE)
+  names(worldclim_all_data) <- gsub("wc2.1_30s_bio_", "worldclim_all.", names(worldclim_all_data))
+  names(worldclim_all_data) <- gsub("wc2.1_30s_elev", "worldclim_all.20", names(worldclim_all_data))
+  worldclim_all_data <- worldclim_all_data[complete.cases(worldclim_all_data), ]
   sps_predictions_maxent <- predict(object = modl, 
                                     newdata = worldclim_all_data, 
                                     clamp = TRUE,
                                     type = c("cloglog")
                                     )
+  rm(worldclim_all_data); gc()
   sps_predictions_maxent <- as.data.table(sps_predictions_maxent)
   head(sps_predictions_maxent)
   range(sps_predictions_maxent)
   nrow(sps_predictions_maxent)
   
-  worldclim_all_data <- as.data.table(as.data.frame(worldclim_all[[1]]))
-  worldclim_all_data$raster_position <- 1:nrow(worldclim_all_data)
+  worldclim_all_data0 <- as.data.table(as.data.frame(worldclim_all[[1]]))
+  worldclim_all_data0$raster_position <- 1:nrow(worldclim_all_data0)
 
-  worldclim_all_data1 <- worldclim_all_data
+  worldclim_all_data1 <- worldclim_all_data0
   worldclim_all_data1 <- worldclim_all_data1[complete.cases(worldclim_all_data1), ]
+  
+  worldclim_all_data0 <- worldclim_all_data0[, .SD, .SDcols = "raster_position"]
   
   worldclim_all_data1[, predictions := sps_predictions_maxent$V1]
   
   
-  worldclim_all_data <- merge(worldclim_all_data[, "raster_position", with = FALSE], 
+  worldclim_all_data0 <- merge(worldclim_all_data0[, "raster_position", with = FALSE], 
                               worldclim_all_data1[, .SD, .SDcols = c("raster_position", "predictions")], 
                               by = "raster_position", all.x = TRUE)
 
+  rm(worldclim_all_data1); gc()
+  
   sps_preds_rstr <- worldclim_all[[1]]
-  sps_preds_rstr <- setValues(sps_preds_rstr, worldclim_all_data$predictions)
+  sps_preds_rstr <- setValues(sps_preds_rstr, worldclim_all_data0$predictions)
+  
+  rm(worldclim_all_data0); gc()
+
   
   #pdf("sps_predictions_maxent_kk.pdf", width = 20, height = 15)
   #par(mfrow = c(1, 2))
@@ -214,7 +234,7 @@ for (t in taxons){
   thr <- rbind(a, b)
   
   sps_preds_rstr_pres_abs <- reclassify(sps_preds_rstr, rcl = thr, filename = '', include.lowest = FALSE, right = TRUE)
-  plot(sps_preds_rstr_pres_abs)
+  #plot(sps_preds_rstr_pres_abs)
   
 
   pdf(paste0(dir2save, "sps_predictions_maxent_", t, ".pdf"), width = 18, height = 15)
